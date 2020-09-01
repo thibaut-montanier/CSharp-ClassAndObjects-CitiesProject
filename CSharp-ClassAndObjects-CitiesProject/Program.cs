4b0ac60e850728248155dccd538d7df333ab3df1
Thibaut Montanier <thibaut.montanier@gmail.com>
2020-09-01 09:40:09 +0200

    Add project files.

---
+++ CSharp-ClassAndObjects-CitiesProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CSharp_ClassAndObjects_CitiesProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
