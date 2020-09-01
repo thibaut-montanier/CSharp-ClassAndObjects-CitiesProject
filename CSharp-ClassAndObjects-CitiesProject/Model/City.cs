504f21e6111a3ccbd7e94a75a01e0d48b5aa1c59
Thibaut Montanier <thibaut.montanier@gmail.com>
2020-09-01 16:44:20 +0200

    Classes-Interface-01-Create Interfaces-exercice-Start

--- CSharp-ClassAndObjects-CitiesProject/Model/City.cs
+++ CSharp-ClassAndObjects-CitiesProject/Model/City.cs
@@ -3,27 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CSharp_POO.Model {
-public class City {
+    public class City {
 
-    /// <summary>
-    /// Nom de la commune
-    /// </summary>
-    public string Name { get; set; }
-    /// <summary>
-    /// Nombre d'habitants
-    /// </summary>
-    public int NbCitizens { get; set; }
-    /// <summary>
-    /// Code postal de la commune
-    /// </summary>
-    public string PostCode { get; set; }
-    /// <summary>
-    /// Departement de la commune
-    /// </summary>
-    public Department Department { get; set; }
-
-
-
-
-}
+        /// <summary>
+        /// Nom de la commune
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Nombre d'habitants
+        /// </summary>
+        public int NbCitizens { get; set; }
+        /// <summary>
+        /// Code postal de la commune
+        /// </summary>
+        public string PostCode { get; set; }
+        /// <summary>
+        /// Departement de la commune
+        /// </summary>
+        public Department Department { get; set; }
+    }
 }
