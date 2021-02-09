9829f52a2639366c9504e9fd7d3064e424258fcc
Thibaut Montanier <thibaut.montanier@gmail.com>
2021-02-09 11:37:00 +0100

    Generics-ETL-AbstractClass

---
+++ CSharp-ClassAndObjects-CitiesProject/Services/ETL/ETLMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_ClassAndObjects_CitiesProject.Services.ETL {
+public abstract class ETLMain<TInput, TOutput> where TInput :class
+                                where TOutput : IDBObject, new() {
+
+    /// <summary>
+    /// Load data : must be override in another class
+    /// </summary>
+    /// <returns></returns>
+    protected abstract IEnumerable<TInput> LoadAll();
+
+    /// <summary>
+    /// TransoformeOne : transfer datas from input to outpu
+    /// </summary>
+    /// <param name="ti"></param>
+    /// <param name="to"></param>
+    protected abstract void TransformeOne(TInput ti, TOutput to);
+
+    /// <summary>
+    /// Transforme all data from source (load) to any destination
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<TOutput> TransformeAll() {
+        int counter = 0;
+        foreach(var ti in LoadAll()) {
+            var to = new TOutput();
+            // counter management for the ID
+            to.ID = ++counter;
+            TransformeOne(ti, to);
+            yield return to;
+        }
+    }
+}
+}
---
+++ CSharp-ClassAndObjects-CitiesProject/Services/ETL/IDBObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_ClassAndObjects_CitiesProject.Services.ETL {
+    public class IDBObject {
+
+        public int ID;
+    }
+}
